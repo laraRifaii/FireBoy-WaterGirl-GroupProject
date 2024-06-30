59553c9b889bb01b1f4b9b684b9405884a569544
Hamzi Haidar <hamzihaider@gmail.com>
2024-06-30 19:23:47 +0300

    fixing title

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-
 let player1 = document.querySelector(".player1");
 let player2 = document.querySelector(".player2");
 let text = document.querySelector("h1");
@@ -9,7 +8,6 @@ window.addEventListener("scroll", () => {
 
   player1.style.left = value * 0.4 + "px";
   player2.style.right = value * 0.4 + "px";
-  // text.style.marginTop = value * 0.8 + "px";
+  text.style.marginTop = value * 0.8 + "px";
   startBtn.style.transform = `scale(${value / 800})`;
 });
-
