8a50c836e52f8bf454b477840594397456b309bf
Hamzi Haidar <hamzihaider@gmail.com>
2024-07-02 23:10:00 +0300

    added more parallax changed draw box size and style

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -2,33 +2,36 @@ let sketch1 = function (p) {
   let character;
 
   p.setup = function () {
-    let canvas = p.createCanvas(80, 137);
+    let canvas = p.createCanvas(150, 200);
     canvas.parent("character1");
     character = {
       headEmoji: "🧑‍🦰",
     };
+    buffer1 = p.createGraphics(150, 200);
   };
 
   p.draw = function () {
+    p.clear();
+    p.image(buffer1, 0, 0);
+
     p.strokeWeight(3);
-    p.textSize(40);
+    p.textSize(60);
 
-    // Draw character 1
-    p.text(character.headEmoji, 14, 40);
-    
+    p.text(character.headEmoji, 35, 60);
+
     p.mouseDragged = () => {
-      p.stroke("#f01e2c");
-      p.ellipse(p.mouseX, p.mouseY, 3, 3);
+      buffer1.stroke("#f01e2c");
+      buffer1.strokeWeight(5);
+      buffer1.ellipse(p.mouseX, p.mouseY, 3, 3);
     };
-  };  
-
-
+  };
 
   function updateCharacter() {
     character.headEmoji = p.select("#headEmoji1").value();
-    }
+  }
 
   p.select("#headEmoji1").changed(updateCharacter);
+  p.select(".player1 .btn-clear").mousePressed(() => buffer1.clear());
 
   p.saveSketch = function () {
     p.saveCanvas("character1.png", "png");
@@ -39,32 +42,36 @@ let sketch2 = function (p) {
   let character;
 
   p.setup = function () {
-    let canvas = p.createCanvas(80, 137);
+    let canvas = p.createCanvas(150, 200);
     canvas.parent("character2");
     character = {
       headEmoji: "👧",
     };
+    buffer2 = p.createGraphics(150, 200);
   };
 
   p.draw = function () {
-    p.strokeWeight(3);
-    p.textSize(40);
+    p.clear();
+    p.image(buffer2, 0, 0);
 
-    p.text(character.headEmoji, 14, 40);
+    p.strokeWeight(3);
+    p.textSize(60);
+
+    p.text(character.headEmoji, 35, 60);
 
     p.mouseDragged = () => {
-      p.stroke("#0268d7");
-      p.ellipse(p.mouseX, p.mouseY, 3, 3);
+      buffer2.stroke("#02b1d7");
+      buffer2.strokeWeight(5);
+      buffer2.ellipse(p.mouseX, p.mouseY, 3, 3);
     };
-  };  
-
- 
+  };
 
   function updateCharacter() {
     character.headEmoji = p.select("#headEmoji2").value();
   }
 
   p.select("#headEmoji2").changed(updateCharacter);
+  p.select(".player2 .btn-clear").mousePressed(() => buffer2.clear());
 
   p.saveSketch = function () {
     p.saveCanvas("character2.png", "png");
